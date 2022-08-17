bd5118a5c9d02dacf2871f5036cd687007fb00b1
Ivan <tornadod@mail.ru>
2022-08-17 17:32:58 +0300

    1

--- src/components/message-list/message-list.js
+++ src/components/message-list/message-list.js
@@ -9,29 +9,34 @@ function ShowTime(){
 
 
 export const MessageList = () => {
+  //привязываем к стету массивы
   const [messageList, setMessageList] = useState([]);
   const [value, setValue] = useState("");
 
+  //добавляем в массив значения, т.к. массив пересоздаётся нужно добавлять предыдущие значения
   const sendMessage = () => {
     if (value) {
       setMessageList([
         ...messageList,
         { author: "User", message: value, date: ShowTime() },
       ]);
+      //обнуляем значение введенной строки
       setValue("");
     }
   };
 
+  //функция обработки нажатия клавишы Enter
   const handlePressInput = ({ code }) => {
     if (code === "Enter") {
       sendMessage();
     }
   };
 
+  //обработка нажатия кнопки на сайте
   useEffect(() => {
     const lastMessage = messageList[messageList.length - 1];
     let timerId = null;
-
+//проверка на пустоту и автора, не нужно отвечать пока не было сообщений от пользователя, и не нужно отвечать повтороно боту
     if (messageList.length && lastMessage.author === "User") {
       timerId = setTimeout(() => {
         setMessageList([
@@ -41,13 +46,16 @@ export const MessageList = () => {
       }, 1500);
 
       return () => {
+        //очищаем таймер
         clearInterval(timerId);
       };
     }
   }, [messageList]);
 
+  //выводится список сообщений
   return (
     <>
+    
       <div>
         {messageList.map((message) => (
           <Message message={message} />
