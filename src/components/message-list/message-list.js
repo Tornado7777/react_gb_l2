5fdbf3fdbb972b35d0f059af47a2e0e796482840
Ivan <tornadod@mail.ru>
2022-08-22 14:03:38 +0300

    старт задание 3

--- src/components/message-list/message-list.js
+++ src/components/message-list/message-list.js
@@ -1,75 +1,91 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+// import PropTypes from "prop-types";
+import { InputAdornment } from "@mui/material";
 import { Message } from "./message";
-
-function ShowTime(){
-  let date = new Date();
-  return date.toLocaleTimeString();
-}
-
-
+import { Input, SendIcon } from "./styles";
 
 export const MessageList = () => {
-  //привязываем к стету массивы
   const [messageList, setMessageList] = useState([]);
   const [value, setValue] = useState("");
 
-  //добавляем в массив значения, т.к. массив пересоздаётся нужно добавлять предыдущие значения
+  const ref = useRef();
+
   const sendMessage = () => {
     if (value) {
       setMessageList([
         ...messageList,
-        { author: "User", message: value, date: ShowTime() },
+        { author: "User", message: value, date: new Date() },
       ]);
-      //обнуляем значение введенной строки
       setValue("");
     }
   };
 
-  //функция обработки нажатия клавишы Enter
   const handlePressInput = ({ code }) => {
     if (code === "Enter") {
       sendMessage();
     }
   };
 
-  //обработка нажатия кнопки на сайте
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.scrollTo({
+        top: ref.current.scrollHeight,
+        left: 0,
+        behavior: "smooth",
+      });
+    }
+  }, [messageList]);
+
   useEffect(() => {
     const lastMessage = messageList[messageList.length - 1];
     let timerId = null;
-//проверка на пустоту и автора, не нужно отвечать пока не было сообщений от пользователя, и не нужно отвечать повтороно боту
+
     if (messageList.length && lastMessage.author === "User") {
       timerId = setTimeout(() => {
         setMessageList([
           ...messageList,
-          { author: "Bot", message: "Hello from Bot", date: ShowTime() },
+          { author: "Bot", message: "Hello from Bot", date: new Date() },
         ]);
-      }, 1500);
+      }, 500);
 
       return () => {
-        //очищаем таймер
         clearInterval(timerId);
       };
     }
   }, [messageList]);
 
-  //выводится список сообщений
   return (
     <>
-    
-      <div>
+      <div ref={ref}>
         {messageList.map((message, index) => (
-          <Message message={message} key = {index}/>
+          <Message message={message} key={index} />
         ))}
       </div>
 
-      <button onClick={sendMessage}>send</button>
-      <input
+      <Input
         fullWidth
         placeholder="Введите сообщение..."
         value={value}
         onChange={(e) => setValue(e.target.value)}
         onKeyPress={handlePressInput}
+        endAdornment={
+          <InputAdornment position="end">
+            {value && <SendIcon onClick={sendMessage} />}
+          </InputAdornment>
+        }
       />
     </>
   );
 };
+
+// MessageList.propTypes = {
+//   message: PropTypes.string.isRequired,
+//   o1: PropTypes.shape({
+//     s1: PropTypes.string.isRequired,
+//   }).isRequired,
+//   a: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       s1: PropTypes.string.isRequired,
+//     }).isRequired
+//   ).isRequired,
+// };
